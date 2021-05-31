fc7b6af841acd1f81540c14b71b11cc7b203a1f4
blopez-dev <pjgl.frontend@gmail.com>
2021-05-31 18:07:17 +0200

    Added frontend architecture project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Auth0Provider } from '@auth0/auth0-react';
 import configureStore from './application/store/index';
 import Application from './application/application';
 
 ReactDOM.render(
   <React.StrictMode>
+  <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
     <Provider store={configureStore()}>
       <Application />
     </Provider>
+  </Auth0Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
