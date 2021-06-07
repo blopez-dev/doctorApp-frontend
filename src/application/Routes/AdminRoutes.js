f815142a59ba39c5ba0de6614a719bdeb8ff51d2
blop-dev <pjgl.frontend@gmail.com>
2021-06-07 10:22:28 +0200

    Added doctors component in main branch

--- src/application/Routes/AdminRoutes.js
+++ src/application/Routes/AdminRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
 import Dashboard from '../../pages/Admin/Dashboard/Dashboard';
+import Doctors from '../../pages/Admin/Doctors/Doctor';
 
 const Routing = () => (
   <Router>
@@ -10,7 +11,9 @@ const Routing = () => (
         <Route exact path="/">
           <Dashboard />
         </Route>
-        <Route path="/doctores" />
+        <Route path="/doctores">
+          <Doctors />
+        </Route>
         <Route path="/pacientes" />
         <Route path="/agenda" />
       </Switch>
